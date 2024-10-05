82fa815b7c6b1b0df53176cf8df23219d2c04f6f
Miguel Almeida <miguelh.almeida05@gmail.com>
2024-10-05 16:14:57 -0300

    separação dos projetos em python e c#

--- sudoku-python/main.py
+++ sudoku-python/main.py
@@ -4,7 +4,6 @@ import random
 
 class SudokuApp:
     def __init__(self, root):
-        print('Codigo na branch do Miguel')
         self.root = root
         self.root.title("Sudoku")
         self.grid = [[0 for _ in range(9)] for _ in range(9)]
