c0c1ef8fc2c899b43d61c82bde185c9c29394567
Pedro Macedo de Sousa <56598017+PedroMac-ss@users.noreply.github.com>
2024-10-12 11:54:12 -0300

    Incluindo estrutura c#

---
+++ CSharpSudoku/src/Sudoku.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SALTO-CC-242-GRUPO-06.CSharpSudoku.src
+{
+    public class Sudoku
+    {
+        print("Hello World!");
+    }
+}
