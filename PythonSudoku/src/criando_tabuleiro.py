c53576504355c612e514f1bb48753cbf8f7448ac
Miguel Almeida <miguelh.almeida05@gmail.com>
2024-11-25 17:11:07 -0300

    atualizado

--- PythonSudoku/src/criando_tabuleiro.py
+++
@@ -1,9 +0,0 @@
-def criar_tabuleiro_vazio():
-    return [[0 for _ in range(9)] for _ in range(9)]
-
-def exibir_tabuleiro(tabuleiro):
-    for linha in tabuleiro:
-        print(linha)
-
-tabuleiro = criar_tabuleiro_vazio()
-exibir_tabuleiro(tabuleiro)
